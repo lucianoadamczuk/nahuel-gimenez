0b779f4c524b6d1f2f1e0d38386b801ac60bff88
Luciano Adamczuk <lucianoadamczuk@gmail.com>
2024-02-29 15:09:53 -0300

    feat: :tada: Add Tailwind, colors and Prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			background: {
+				DEFAULT: '#F2F2F2',
+				gray: '#595959',
+				dark: '#262626',
+			},
+
+			light: '#F2F2F2',
+			dark: '#262626',
+
+			primary: {
+				DEFAULT: '#593723',
+				light: '#8C5E35',
+			},
+
+			highlight: {
+				DEFAULT: '#8C1E14',
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
